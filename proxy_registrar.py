5c59aa2df2ab1085b6814052a5891c343cc2ab85
Luis Galán <luisgalanramil@gmail.com>
2017-01-10 20:31:53 +0100

    haciendo json

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -11,6 +11,7 @@ import time
 import xml.etree.ElementTree as ET
 import random
 import hashlib
+import time
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
@@ -19,7 +20,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     dicc = {}
     expire = ''
-    lists = []
+    users = []
     direction = ''
     nonce = ''
     for i in range (10):
@@ -48,13 +49,27 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
 
             data = info.split(':')
-            user = data[1]
-            print(user + 'UEEEEEEEEEEEEEEEEEEE')
+
+            self.user = data[1]
+            self.ip = self.client_address[0]
+            print(self.ip)
+            p = data[2].split(' ')
+            self.port = p[0]
+            self.regdate = time.time()
+
+            e = data[3].split('\r\n')
+            e1 = e[0].split(' ')
+            self.expire = e1[1]
+
+
+            self.register2json()
+
+            print(self.user + 'UEEEEEEEEEEEEEEEEEEE')
             FILE = open('passwords.txt')
             for line in FILE:
                 miau = line.split(',')
                 print(miau)
-                if user == miau[0]:
+                if self.user == miau[0]:
                     print('SÍ QUE ESTÁ\r\n')
                     self.password = miau[1]
                     print(self.password)
@@ -68,13 +83,38 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             print(response)
             if str(m2) == str(response):
                 print('ESTÁ')
+#                register2json()
+
         else:
             print(self.nonce)
             self.wfile.write(b'SIP/2.0 401 Unauthorized\r\nWWW Authenticate: \
 Digest nonce=' + bytes(self.nonce,'utf-8'))
 
+    def register2json(self):
+        """
+        Método para almacenar correctamente los datos al json
+        """
+
+        auxdicc = {'address': self.user, 'ip': self.ip, 'port': self.port,'regdate': self.regdate, 'expires': self.expire}
+
+#        for i in self.users:
+#            if i['address'] == self.direction:
+#                self.users.remove(l)
+
+        self.users.append(auxdicc)
+        exptime = time.strftime('%Y-%m-%d %H:%M:%S',time.gmtime(int(self.expire) + time.time()))
+
+#        for l in self.users:
+#            if l[1]['expires'] <= time.strftime('%Y-%m-%d %H:%M:%S',
+#                                                time.gmtime(time.time())):
+#            self.users.remove(l)
+
+        json.dump(self.users, open("registered.json", 'w'),
+                  sort_keys=True, indent=4, separators=(',', ': '))
+
+
 '''
-        if self.lists == []:
+        if self.users == []:
             self.json2registered()
 
         print(self.client_address)
@@ -96,32 +136,13 @@ Digest nonce=' + bytes(self.nonce,'utf-8'))
         self.wfile.write(b"SIP/2.0 200 OK " + b'\r\n\r\n')
         print(self.dicc)
 
-    def register2json(self):
-        """
-        Método para almacenar correctamente los datos al json
-        """
-        exptime = time.strftime('%Y-%m-%d %H:%M:%S',
-                                time.gmtime(int(self.expire) + time.time()))
-        auxdicc = {'address': self.client_address[0], 'expires': exptime}
-
-        for l in self.lists:
-            if l[0] == self.direction:
-                self.lists.remove(l)
-        self.lists.append([self.direction, auxdicc])
-
-        for l in self.lists:
-            if l[1]['expires'] <= time.strftime('%Y-%m-%d %H:%M:%S',
-                                                time.gmtime(time.time())):
-                self.lists.remove(l)
-        json.dump(self.lists, open("registered.json", 'w'),
-                  sort_keys=True, indent=4, separators=(',', ': '))
 
     def json2registered(self):
         """
         Método para comprobar al inicio si ya existía un json
         """
         try:
-            self.lists = json.load(open("registered.json", 'r'))
+            self.users = json.load(open(REGUSERS, 'r'))
         except:
             pass
 '''
